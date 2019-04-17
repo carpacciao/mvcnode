c49307504aa4f883ba4f548fec2e039c173f13e8
Lucas <rezaatv@gmail.com>
2019-04-17 10:32:42 +0200

    added basic jwt with hapi-jwt

--- server.js
+++ server.js
@@ -5,8 +5,22 @@ const Hoek = require('hoek')
 const mongoose = require('mongoose')
 const Settings = require('./settings/settings')
 
-console.log(Settings)
-
+/* VALIDATE FOR JWT */
+const validate = async function (decoded, request) {
+  // do your checks to see if the person is valid
+  return {
+    isValid: true
+  }
+  // if (!people[decoded.id]) {
+  //   return {
+  //     isValid: false
+  //   };
+  // } else {
+  //   return {
+  //     isValid: true
+  //   };
+  // }
+};
 /* BASIC SERVER */
 const init = async () => {
   const server = new Hapi.Server({
@@ -17,17 +31,28 @@ const init = async () => {
   server.route({
     method: 'GET',
     path: '/',
+    config: {auth: 'jwt'},
     handler: (request, reply) => {
-      return 'hello'
+      return true
     },
   })
+  //auth jwt
+  await server.register(require('hapi-auth-jwt2'));
+  server.auth.strategy('jwt', 'jwt', {
+    key: 'NeverShareYourSecret', // Never Share your secret key
+    validate: validate, // validate function defined above
+    verifyOptions: {
+      algorithms: ['HS256']
+    } // pick a strong algorithm
+  });
+   server.auth.default('jwt');
+
   await server.start()
-  console.log('Server is running at: ' + server.info.uri)
+  return server
 }
 
-process.on('unhandledRejection', (err) => {
+init().then(server => {
+  console.log('Server is running at: ' + server.info.uri)
+}).catch(err => {
   Hoek.assert(!err, err)
-  process.exit(1);
-});
-
-init()
+})
